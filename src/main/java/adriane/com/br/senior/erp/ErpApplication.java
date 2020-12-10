ae465d5dafa183fc2afd13e33265d2db02944020
Adriane <rodrigues.adriane@outlook.com>
2020-12-10 16:52:20 -0300

    Primeiro commit

---
+++ src/main/java/adriane/com/br/senior/erp/ErpApplication.java
@@ -0,0 +1,13 @@
+package adriane.com.br.senior.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpApplication.class, args);
+	}
+
+}
